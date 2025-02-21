00980d29d7eefca3e19a3b8c0c40799e38ba87ed
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-02-21 23:52:07 +0900

    refactor(view): `OutputView` 최적화 및 코드 가독성 향상
    - `NUMBER_FORMAT`으로 상수 네이밍 변경하여 컨벤션 준수
    - `Arrays.stream()` 대신 `List.of().stream()` 사용하여 불필요한 변환 제거
    - `formatLottoRankString()` 메서드를 분리하여 중복 코드 제거 및 가독성 향상
    - `printProfitRate()` 메서드 분리하여 `printResults()` 가독성 개선

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,17 +1,17 @@
 package lotto.view;
 
 import java.util.Comparator;
+import java.util.stream.Stream;
 import lotto.model.dto.LottoResultDto;
 import lotto.enums.LottoRank;
 import lotto.model.domain.LottoTicket;
 
 import java.text.NumberFormat;
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private static final NumberFormat numberFormat = NumberFormat.getInstance(Locale.KOREA);
+    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance(Locale.KOREA);
 
     public void printLottoTickets(LottoTicket lottoTicket) {
         System.out.printf("%d개를 구매했습니다.%n", lottoTicket.getTickets().size());
@@ -23,18 +23,26 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.print(formatLottoRank(result));
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", result.getProfitRate(purchaseAmount));
+        printProfitRate(result, purchaseAmount);
     }
 
     private String formatLottoRank(LottoResultDto result) {
-        return Arrays.stream(LottoRank.values())
+        return Stream.of(LottoRank.values())
                 .filter(rank -> rank != LottoRank.NONE)
-                .sorted(Comparator.comparingInt(LottoRank::getMatchCount))
-                .map(rank -> String.format("%d개 일치%s (%s원) - %d개%n",
-                        rank.getMatchCount(),
-                        rank == LottoRank.SECOND ? ", 보너스 볼 일치" : "",
-                        numberFormat.format(rank.getPrize()),
-                        result.resultMap().getOrDefault(rank, 0)))
+                .sorted(Comparator.comparingInt(LottoRank::getMatchCount).reversed())
+                .map(rank -> formatLottoRankString(rank, result))
                 .collect(Collectors.joining());
+    }
+
+    private String formatLottoRankString(LottoRank rank, LottoResultDto result) {
+        return String.format("%d개 일치%s (%s원) - %d개%n",
+                rank.getMatchCount(),
+                rank == LottoRank.SECOND ? ", 보너스 볼 일치" : "",
+                NUMBER_FORMAT.format(rank.getPrize()),
+                result.resultMap().getOrDefault(rank, 0));
+    }
+
+    private void printProfitRate(LottoResultDto result, int purchaseAmount) {
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", result.getProfitRate(purchaseAmount));
     }
 }
